28512b5042f31cd240a6420417cd200133b389ec
LAPTOP-GAB3DDT3\liuwei <1454294995@qq.com>
2023-06-03 12:51:29 +0800

    chore: 引入 element plus 设置自动按需导入、类型提示、组件国际化配置、自动导入 ElMessage 等组件的样式

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-expect-error
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
+const locale = zhCn
+</script>
 
 <template>
-  <div></div>
+  <el-config-provider :locale="locale">
+    <div></div>
+  </el-config-provider>
 </template>
 
 <style scoped></style>
